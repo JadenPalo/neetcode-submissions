06a4184a4a9942292d0927f509e071fc437256ac
JadenPalo <palominoja3@gmail.com>
2026-07-16 13:39:14 -0700

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> Groups;
+        for (const auto& s : strs){
+            string sortedStr = s;
+            sort(sortedStr.begin(), sortedStr.end());
+            Groups[sortedStr].push_back(s);
+        }
+        vector<vector<string>> result;
+        for (auto& pair : Groups) {
+            result.push_back(pair.second);
+        }
+        return result;
+    }
+};
